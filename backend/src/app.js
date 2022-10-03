6ca20e9d05353d13ba30ffbbaffac66a3415532c
simonre149 <simonre149@gmail.com>
2022-10-03 17:06:22 +0200

    feat(US7): packages + archi + env

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,31 @@
-// app.js
 const express = require("express")
-
 const app = express()
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const cors = require("cors")
+let corsOptions = { origin: "http://localhost:8081" }
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const dbConfig = require("./config/db.config")
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+})
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// TODO: add models here
 
 module.exports = app
 
 app.get("/", (req, res) => {
-    res.send("Hello World ! ")
-  })
+    res.json({ message: "Coucou c'est du JSON" }) // TODO: remove this line
+})
--- backend/src/server-local.js
+++ backend/src/server-local.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: './.env.local' })
 const app = require("./app")
 const port = process.env.PORT || 4000
 
