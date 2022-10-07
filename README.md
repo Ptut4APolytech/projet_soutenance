4f7e1ad5ade3d539a05275d2d1230a2deca87cd2
p1907112 <clara.beal@etu.univ-lyon1.fr>
2022-10-07 11:12:18 +0200

    feat(10)-set-up-DB-and-Sequelize

--- README.md
+++ README.md
@@ -4,5 +4,11 @@ Se placer dans le dossier back-end : `cd back-end`\
 Lancer Express en local : `npm start`\
 Lancer Express en production : `npm run deployed`
 
+### Sequelize
+Créer un model et sa migration associée : `npx sequelize-cli model:generate --name NomDuModel --attributes label:string`\
+Lancer les migrations en local : `npx dotenv -e .env.local sequelize db:migrate`\
+Lancer les migrations en test : `npx dotenv -e .env.test sequelize db:migrate`\
+Lancer les migrations en prod : `npx dotenv -e .env sequelize db:migrate`
+
 ## Front-end
 _TODO_
--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,11 +1,39 @@
+require('dotenv').config();
+
+
 module.exports = {
-    HOST: process.env.HOST,
-    USER: process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    DB: process.env.DB_NAME,
-    dialect: "mysql",
-    pool: {
-        acquire: 30000,
-        idle: 10000,
-    }
-}
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        pool: {
+            acquire: 30000,
+            idle: 10000,
+        },
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        pool: {
+            acquire: 30000,
+            idle: 10000,
+        },
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        logging: false,
+        pool: {
+            acquire: 30000,
+            idle: 10000,
+        },
+    },
+};
