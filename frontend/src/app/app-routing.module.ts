e39cdd1aee57ce66294e2025f4fd08802a232cb9
simonre149 <Epulapp@EPULAPP2102.univ-lyon1.fr>
2022-11-14 15:44:32 +0100

    feat(US5): admin page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginViewComponent} from "./login/login-view/login-view.component";
 import {ResetPasswordViewComponent} from "./login/reset-password-view/reset-password-view.component";
 import {LoginFirstComponent} from "./login/login-first/login-first.component";
+import {AdminComponent} from "./admin/admin.component";
 
 const routes: Routes = [
   {path:"", component:LoginViewComponent},
   {path:"first-login", component:LoginFirstComponent},
-  {path:"reset-password", component:ResetPasswordViewComponent}
+  {path:"reset-password", component:ResetPasswordViewComponent},
+  {path: "admin", component: AdminComponent},
 ];
 
 @NgModule({
