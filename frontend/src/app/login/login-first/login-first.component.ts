a31f6ce56bb0a08f4483b995956a6e4d544c6b79
simonre149 <simonre149@gmail.com>
2022-11-14 12:24:28 +0100

    wip: form front validation

--- frontend/src/app/login/login-first/login-first.component.ts
+++ frontend/src/app/login/login-first/login-first.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {CustomFormValidators} from "../../form/custom-form-validators";
 
 @Component({
   selector: 'app-login-first',
@@ -14,11 +15,17 @@ export class LoginFirstComponent implements OnInit {
   }
 
   form: FormGroup = new FormGroup({
-    password: new FormControl(''),
-    passwordConfirm: new FormControl(''),
-  });
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=\\S+$).{8,}$')
+    ]),
+    passwordConfirm: new FormControl('', Validators.required),
+  },
+    [CustomFormValidators.matchValidator("password", "passwordConfirm")]
+  );
 
   submit() {
-    console.log("first login form submitted");
+
   }
 }
