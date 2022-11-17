4ff331a790e3a02982853bbdbbe3855b2f71a2c0
simonre149 <Epulapp@EPULAPP2102.univ-lyon1.fr>
2022-11-17 09:01:32 +0100

    wip: make it dynamic

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  @Output() series = this.getSeries();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getSeries() {
+    return [
+      {
+        id: 1,
+        name: 'INFO 5A APP 2021-2024',
+        start: '21/08/2024',
+        end: '25/08/2024',
+        nbApprentices: 26,
+        nbMapsWithConstraints: 19,
+      },{
+        id: 2,
+        name: 'INFO 4A APP 2022-2025',
+        start: '12/08/2024',
+        end: '13/08/2024',
+        nbApprentices: 15,
+        nbMapsWithConstraints: 7,
+      },
+    ];
+  }
 }
