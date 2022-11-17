92fbc53fb9612da4c957d6e47b884f154b00eb1d
simonre149 <Epulapp@EPULAPP2102.univ-lyon1.fr>
2022-11-17 09:31:48 +0100

    wip: add button

--- frontend/src/app/admin/admin-overview/admin-overview.component.ts
+++ frontend/src/app/admin/admin-overview/admin-overview.component.ts
@@ -3,7 +3,7 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'app-admin-overview',
   templateUrl: './admin-overview.component.html',
-  styleUrls: ['./admin-overview.component.scss']
+  styleUrls: ['./admin-overview.component.scss', '../admin.component.scss']
 })
 export class AdminOverviewComponent implements OnInit {
   @Input() serie: any;
