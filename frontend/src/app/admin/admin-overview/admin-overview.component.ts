077df4619e0935e4fd17db33217a3b0df876885d
simonre149 <45072895+simonre149@users.noreply.github.com>
2022-11-17 11:20:25 +0100

    feat(US5) - admin page (#33)
    * feat(US5): admin page
    
    * wip: card
    
    * feat(US10): set up Sequelize & create MDD
    
    * wip: make it dynamic
    
    * wip: add button
    
    * feat(US43): add tailwind (#44)
    
    Authored-by: simonre149
    
    * feat(US5): admin page
    
    * wip: card
    
    * wip: make it dynamic
    
    * wip: add button
    
    * wip: tailwind + css simplifications
    
    * wip: change add serie btn
    
    Co-authored-by: simonre149 <Epulapp@EPULAPP2102.univ-lyon1.fr>
    Co-authored-by: Robin Lusson <roblusson40@gmail.com>
    Co-authored-by: Nicedevil0 <80401190+Nicedevil0@users.noreply.github.com>

---
+++ frontend/src/app/admin/admin-overview/admin-overview.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-admin-overview',
+  templateUrl: './admin-overview.component.html',
+  styleUrls: ['./admin-overview.component.scss', '../admin.component.scss']
+})
+export class AdminOverviewComponent implements OnInit {
+  @Input() serie: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/admin/admin.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent implements OnInit {
+  @Output() series = this.getSeries();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getSeries() {
+    return [
+      {
+        id: 1,
+        name: 'INFO 5A APP 2021-2024',
+        start: '21/08/2024',
+        end: '25/08/2024',
+        nbApprentices: 26,
+        nbMapsWithConstraints: 19,
+      },{
+        id: 2,
+        name: 'INFO 4A APP 2022-2025',
+        start: '12/08/2024',
+        end: '13/08/2024',
+        nbApprentices: 15,
+        nbMapsWithConstraints: 7,
+      },
+    ];
+  }
+}
