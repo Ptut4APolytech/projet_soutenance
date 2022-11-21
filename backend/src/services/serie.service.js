02cc1f44b4ea24d41ba1a497f3950f8199d9ac4d
Robin LUSSON <roblusson40@gmail.com>
2022-11-21 15:34:42 +0100

    feat(US40): create serieSoutenance

--- backend/src/services/serie.service.js
+++ backend/src/services/serie.service.js
@@ -6,41 +6,34 @@ const jurorService = require("../services/juror.service");
 const roomService = require("../services/room.service");
 
 exports.create = (serie) => {
-  db.series
-    .create({ label: serie.label })
-    .then(async (res) => {
-      await slotService.createMany(serie.slots, res.id).then((r) => {
-        res.slots = r;
-      });
+    db.series.create({ label: serie.label })
+        .then(async (res) => {
+            await slotService.createMany(serie.slots, res.id)
+                .then((r) => { res.slots = r; });
 
-      await roomService.createMany(serie.rooms, res.id).then((r) => {
-        res.rooms = r;
-      });
+            await roomService.createMany(serie.rooms, res.id)
+                .then((r) => { res.rooms = r; });
 
-      await jurorService.createMany(serie.jurors, res.id).then((r) => {
-        res.jurors = r;
-      });
+            await jurorService.createMany(serie.jurors, res.id)
+                .then((r) => { res.jurors = r; });
 
-      serie.students.forEach((student) => {
-        let jurors = serie.jurors.findAll(
-          (j) => j.studentNumber == student.number
-        );
-        student.masterId = res.jurors.find(
-          (j) => j.dataValues.email == jurors.find((j2) => j2.master).email
-        ).id;
+            serie.students.forEach(student => {
+                let jurors = serie.jurors.findAll(j => j.studentNumber == student.number);
+                student.masterId = res.jurors
+                    .find(j => j.dataValues.email == jurors.find(j2 => j2.master).email)
+                    .id;
+                
+                student.tutorId = res.jurors
+                .find(j => j.dataValues.email == jurors.find(j2 => !j2.master).email)
+                .id;
+            });
 
-        student.tutorId = res.jurors.find(
-          (j) => j.dataValues.email == jurors.find((j2) => !j2.master).email
-        ).id;
-      });
+            await studentService.createMany(serie.students, res.id)
+                .then((r) => { res.students = r; });
 
-      await studentService.createMany(serie.students, res.id).then((r) => {
-        res.students = r;
-      });
-
-      return res;
-    })
-    .catch((err) => {
-      throw err;
-    });
+            return res;
+        })
+        .catch((err) => {
+            throw err;
+        });
 };
--- backend/src/services/slot.service.js
+++ backend/src/services/slot.service.js
@@ -1,8 +1,8 @@
 const db = require("../models");
 
 exports.createMany = (slots, serieId) => {
-  slots.forEach((slot) => {
-    slot.serieId = serieId;
-  });
-  return db.slots.bulkCreate(slots);
+    slots.forEach(slot => {
+        slot.serieId = serieId;
+    });
+    return db.slots.bulkCreate(slots);
 };
