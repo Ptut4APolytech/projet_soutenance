4e7dbb129d4c386677d9f12bd5b61428b86fcf64
PaulineFR <pauline.franquelin@gmail.com>
2023-01-02 19:53:21 +0100

    feat(US64): planning functions definitions

--- backend/src/services/planning.service.js
+++ backend/src/services/planning.service.js
@@ -19,3 +19,59 @@ exports.build = async (id) => {
 
     return students;
   };
+
+
+exports.checkJury = () => {
+  return [];
+  // FORMAT TABLEAU RETOURNÉ
+  /*
+  [
+    {
+      idJury : 01,
+      errorCode: 301, => créer son propre code erreur
+    },
+    {
+      idJury : 01,
+      errorCode: 302,
+    }
+  ]
+  */
+}
+
+exports.checkJurorSlot = (idJuror, idSlot) => {
+  return true;
+  /* return true si juré disponnible sur ce créenau horaire, false sinon */
+}
+
+exports.dispoJury = (idJury) => {
+  return [];
+  /* le tableau de la liste des slots disponnibles entre les trois jurés du juré */
+  /* erreur si tableau vide (plus tard) */
+}
+
+exports.checkSlots = () => {
+  return true;
+  /* return true si le nombre de salles x le nombre de slots >= nopbre d'étudiants, false sinon */
+  /* erreur si false (plus tard) */
+}
+
+exports.orderJury = () => {
+  return [];
+  /* retourne un tableau de liste d'id des jury triés (jury avec les MAP qui s'occupent de beaucoup d'étudiant en premier...)*/
+}
+
+exports.checkPlanning = () => {
+  return [];
+  // FORMAT TABLEAU RETOURNÉ (à mieux refléchir les codes d'erreurs)
+  /*
+  [
+    {
+      errorCode: 301, => créer son propre code erreur
+    }
+  ]
+  */
+}
+
+/*
+  FONCTION PLACER LES JURY DANS LES SALLES
+*/
